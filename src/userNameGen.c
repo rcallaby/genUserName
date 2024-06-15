56a3cdf93876a712ceaf2e0e7ca4b4087dc51ba4
rcallaby <rcallaby@gmail.com>
2024-06-14 22:30:38 -0500

    updated generated usernames function

--- src/userNameGen.c
+++ src/userNameGen.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include "userNameGen.h"
 
-#define MAX_NAMES 1000
+#define MAX_NAMES 100
 #define MAX_NAME_LENGTH 50
 #define MAX_USERNAMES 1000
 
@@ -44,17 +44,17 @@ void generateUsernames(char names[MAX_NAMES][MAX_NAME_LENGTH], int name_count, c
         last_lower[strlen(last)] = '\0';
 
         // Generate usernames
-        sprintf(usernames[(*username_count)++], "%s%s", first);
-        sprintf(usernames[(*username_count)++], "%s%s", last);
-        sprintf(usernames[(*username_count)++], "%s%s%s", first, last);
-        sprintf(usernames[(*username_count)++], "%s.%s%s", first, last);
-        sprintf(usernames[(*username_count)++], "%c%s%s", first[0], last);
-        sprintf(usernames[(*username_count)++], "%s%c%s", first, last[0]);
-        sprintf(usernames[(*username_count)++], "%c.%s%s", first[0], last);
-        sprintf(usernames[(*username_count)++], "%s.%c%s", first, last[0]);
-        sprintf(usernames[(*username_count)++], "%s_%s%s", first, last);
-        sprintf(usernames[(*username_count)++], "%c_%s%s", first[0], last);
-        sprintf(usernames[(*username_count)++], "%s_%c%s", first, last[0]);
+        sprintf(usernames[(*username_count)++], "%s", first);
+        sprintf(usernames[(*username_count)++], "%s", last);
+        sprintf(usernames[(*username_count)++], "%s%s", first, last);
+        sprintf(usernames[(*username_count)++], "%s.%s", first, last);
+        sprintf(usernames[(*username_count)++], "%c%s", first[0], last);
+        sprintf(usernames[(*username_count)++], "%s%c", first, last[0]);
+        sprintf(usernames[(*username_count)++], "%c.%s", first[0], last);
+        sprintf(usernames[(*username_count)++], "%s.%c", first, last[0]);
+        sprintf(usernames[(*username_count)++], "%s_%s", first, last);
+        sprintf(usernames[(*username_count)++], "%c_%s", first[0], last);
+        sprintf(usernames[(*username_count)++], "%s_%c", first, last[0]);
 
         sprintf(usernames[(*username_count)++], "%s%s", first, date);
         sprintf(usernames[(*username_count)++], "%s%s", last, date);
@@ -68,17 +68,17 @@ void generateUsernames(char names[MAX_NAMES][MAX_NAME_LENGTH], int name_count, c
         sprintf(usernames[(*username_count)++], "%c_%s%s", first[0], last, date);
         sprintf(usernames[(*username_count)++], "%s_%c%s", first, last[0], date);
 
-        sprintf(usernames[(*username_count)++], "%s%s", first_upper);
-        sprintf(usernames[(*username_count)++], "%s%s", last_upper);
-        sprintf(usernames[(*username_count)++], "%s%s%s", first_upper, last_upper);
-        sprintf(usernames[(*username_count)++], "%s.%s%s", first_upper, last_upper);
-        sprintf(usernames[(*username_count)++], "%c%s%s", first_upper[0], last_upper);
-        sprintf(usernames[(*username_count)++], "%s%c%s", first_upper, last_upper[0]);
-        sprintf(usernames[(*username_count)++], "%c.%s%s", first_upper[0], last_upper);
-        sprintf(usernames[(*username_count)++], "%s.%c%s", first_upper, last_upper[0]);
-        sprintf(usernames[(*username_count)++], "%s_%s%s", first_upper, last_upper);
-        sprintf(usernames[(*username_count)++], "%c_%s%s", first_upper[0], last_upper);
-        sprintf(usernames[(*username_count)++], "%s_%c%s", first_upper, last_upper[0]);
+        sprintf(usernames[(*username_count)++], "%s", first_upper);
+        sprintf(usernames[(*username_count)++], "%s", last_upper);
+        sprintf(usernames[(*username_count)++], "%s%s", first_upper, last_upper);
+        sprintf(usernames[(*username_count)++], "%s.%s", first_upper, last_upper);
+        sprintf(usernames[(*username_count)++], "%c%s", first_upper[0], last_upper);
+        sprintf(usernames[(*username_count)++], "%s%c", first_upper, last_upper[0]);
+        sprintf(usernames[(*username_count)++], "%c.%s", first_upper[0], last_upper);
+        sprintf(usernames[(*username_count)++], "%s.%c", first_upper, last_upper[0]);
+        sprintf(usernames[(*username_count)++], "%s_%s", first_upper, last_upper);
+        sprintf(usernames[(*username_count)++], "%c_%s", first_upper[0], last_upper);
+        sprintf(usernames[(*username_count)++], "%s_%c", first_upper, last_upper[0]);
 
         sprintf(usernames[(*username_count)++], "%s%s", first_upper, date);
         sprintf(usernames[(*username_count)++], "%s%s", last_upper, date);
@@ -92,17 +92,17 @@ void generateUsernames(char names[MAX_NAMES][MAX_NAME_LENGTH], int name_count, c
         sprintf(usernames[(*username_count)++], "%c_%s%s", first_upper[0], last_upper, date);
         sprintf(usernames[(*username_count)++], "%s_%c%s", first_upper, last_upper[0], date);
 
-        sprintf(usernames[(*username_count)++], "%s%s", first_lower);
-        sprintf(usernames[(*username_count)++], "%s%s", last_lower);
-        sprintf(usernames[(*username_count)++], "%s%s%s", first_lower, last_lower);
-        sprintf(usernames[(*username_count)++], "%s.%s%s", first_lower, last_lower);
-        sprintf(usernames[(*username_count)++], "%c%s%s", first_lower[0], last_lower);
-        sprintf(usernames[(*username_count)++], "%s%c%s", first_lower, last_lower[0]);
-        sprintf(usernames[(*username_count)++], "%c.%s%s", first_lower[0], last_lower);
-        sprintf(usernames[(*username_count)++], "%s.%c%s", first_lower, last_lower[0]);
-        sprintf(usernames[(*username_count)++], "%s_%s%s", first_lower, last_lower);
-        sprintf(usernames[(*username_count)++], "%c_%s%s", first_lower[0], last_lower);
-        sprintf(usernames[(*username_count)++], "%s_%c%s", first_lower, last_lower[0]);
+        sprintf(usernames[(*username_count)++], "%s", first_lower);
+        sprintf(usernames[(*username_count)++], "%s", last_lower);
+        sprintf(usernames[(*username_count)++], "%s%s", first_lower, last_lower);
+        sprintf(usernames[(*username_count)++], "%s.%s", first_lower, last_lower);
+        sprintf(usernames[(*username_count)++], "%c%s", first_lower[0], last_lower);
+        sprintf(usernames[(*username_count)++], "%s%c", first_lower, last_lower[0]);
+        sprintf(usernames[(*username_count)++], "%c.%s", first_lower[0], last_lower);
+        sprintf(usernames[(*username_count)++], "%s.%c", first_lower, last_lower[0]);
+        sprintf(usernames[(*username_count)++], "%s_%s", first_lower, last_lower);
+        sprintf(usernames[(*username_count)++], "%c_%s", first_lower[0], last_lower);
+        sprintf(usernames[(*username_count)++], "%s_%c", first_lower, last_lower[0]);
 
         sprintf(usernames[(*username_count)++], "%s%s", first_lower, date);
         sprintf(usernames[(*username_count)++], "%s%s", last_lower, date);
